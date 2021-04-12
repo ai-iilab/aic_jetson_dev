484caab0273e6292bdc761f492ada2e361bfc07b
Seungho Lee <seunghl@keti.re.kr>
2021-04-12 17:26:38 +0900

    Cleanup code

--- InferenceTRT.py
+++ InferenceTRT.py
@@ -55,7 +55,6 @@ class InferenceTRT(object):
                 host_outputs.append(host_mem)
         
         self.context = context
-        self.engine = engine
         self.input_ptr = input_ptr
         self.cuda_inputs = cuda_inputs
         self.cuda_outputs = cuda_outputs
