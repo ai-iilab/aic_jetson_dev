fde7a6c7a108d1451b7faa9a1270b2372c65ce39
Seungho Lee <seunghl@keti.re.kr>
2021-04-12 17:44:08 +0900

    Add "Main.py" file

---
+++ Main.py
@@ -0,0 +1,11 @@
+"""
+File: Main.py
+
+Authors: Jinwoo Jeong <jw.jeong@keti.re.kr>
+         Sungjei Kim <sungjei.kim@keti.re.kr>
+         Seungho Lee <seunghl@keti.re.kr>
+
+The property of program is under Korea Electronics Technology Institute.
+For more information, contact us at <jw.jeong@keti.re.kr>.
+"""
+
