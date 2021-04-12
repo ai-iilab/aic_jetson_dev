501040018406d26081cc5797f49b3f0fb1f28cc3
Seungho Lee <seunghl@keti.re.kr>
2021-04-12 15:46:46 +0900

    Define "PreProcessor" class

--- PreProcessor.py
+++ PreProcessor.py
@@ -8,3 +8,14 @@ Authors: Jinwoo Jeong <jw.jeong@keti.re.kr>
 The property of program is under Korea Electronics Technology Institute.
 For more information, contact us at <jw.jeong@keti.re.kr>.
 """
+
+class PreProcessor(object):
+    """
+    description: A PreProcessor class that warps preprocess ops.
+    """
+    
+    def __init__(self):
+        print("PreProcessor init")
+        
+    def destroy(self):
+        print("PreProcessor destroy")
