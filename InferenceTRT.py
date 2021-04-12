9efedfbd7319209593d6919236d448cf832a5e00
Seungho Lee <seunghl@keti.re.kr>
2021-04-12 16:51:10 +0900

    Implement for usage of CUDA

--- InferenceTRT.py
+++ InferenceTRT.py
@@ -9,6 +9,9 @@ The property of program is under Korea Electronics Technology Institute.
 For more information, contact us at <jw.jeong@keti.re.kr>.
 """
 
+import pycuda.autoinit
+import pycuda.driver as cuda
+
 class InferenceTRT(object):
     """
     description: A InferenceTRT class that warps TensorRT ops
@@ -17,5 +20,11 @@ class InferenceTRT(object):
     def __init__(self):
         print("InferenceTRT init")
         
+        self.cfx = cuda.Device(0).make_context()
+        
+        self.context = context
+        
     def destroy(self):
         print("InferenceTRT destroy")
+        
+        self.cfx.pop()
