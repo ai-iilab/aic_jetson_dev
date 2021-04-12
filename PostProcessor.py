a54f0d267ab0a54794e12d92a69c6aaf7c38cb6f
Seungho Lee <seunghl@keti.re.kr>
2021-04-12 17:22:33 +0900

    Define PostProcessor class

--- PostProcessor.py
+++ PostProcessor.py
@@ -8,3 +8,14 @@ Authors: Jinwoo Jeong <jw.jeong@keti.re.kr>
 The property of program is under Korea Electronics Technology Institute.
 For more information, contact us at <jw.jeong@keti.re.kr>.
 """
+
+class PostProcessor(object):
+    """
+    description: A PostProcessor class that warps postprocess ops.
+    """
+    
+    def __init__(self):
+        print("PostProcessor init")
+    
+    def destroy(self):
+        print("PostProcessor destroy")
