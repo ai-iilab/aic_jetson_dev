16c629d46ea5589e0eb89963d32ae05270e0d4bf
Seungho Lee <seunghl@keti.re.kr>
2021-04-12 15:42:39 +0900

    Add "PreProcessor.py" file

---
+++ PreProcessor.py
@@ -0,0 +1,10 @@
+"""
+File: PreProcessor.py
+
+Authors: Jinwoo Jeong <jw.jeong@keti.re.kr>
+         Sungjei Kim <sungjei.kim@keti.re.kr>
+         Seungho Lee <seunghl@keti.re.kr>
+
+The property of program is under Korea Electronics Technology Institute.
+For more information, contact us at <jw.jeong@keti.re.kr>.
+"""
