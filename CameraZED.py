5353fa76e662be97f5736a01ba05d90738d64ae4
Seungho Lee <seunghl@keti.re.kr>
2021-07-09 13:41:40 +0900

    Implement CameraZED class

--- CameraZED.py
+++ CameraZED.py
@@ -36,6 +36,13 @@ class CameraZED(object):
         self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.width * 2)
         self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self.height)
         self.cap.set(cv2.CAP_PROP_FPS, self.fps)
+        
+        calibration_file = self.download_calibration_file(self.serial_number)
+        if calibration_file  == "":
+            exit(1)
+        
+        print("Calibration file found. Loading...")
+        camera_matrix_left, camera_matrix_right, map_left_x, map_left_y, map_right_x, map_right_y = self.init_calibration(calibration_file)
     
     def destroy(self):
         print("CameraZED destroy")
