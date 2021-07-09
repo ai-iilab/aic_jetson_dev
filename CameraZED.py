a5690f66bc65c3b0f6c8314717da211e04860fd3
Seungho Lee <seunghl@keti.re.kr>
2021-07-09 13:38:12 +0900

    Implement CameraZED class

--- CameraZED.py
+++ CameraZED.py
@@ -9,3 +9,20 @@ The property of program is under Korea Electronics Technology Institute.
 For more information, contact us at <jw.jeong@keti.re.kr>.
 """
 
+import sys
+import os
+import numpy as np
+import configparser
+import cv2
+import wget
+
+class CameraZED(object):
+    """
+    description: A CameraZED class that warps image capture ops.
+    """
+    
+    def __init__(self):
+        print("CameraZED init")
+    
+    def destroy(self):
+        print("CameraZED destroy")
