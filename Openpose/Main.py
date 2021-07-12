48231000fe570cae31f90e94abc5edd733a0bace
Seungho Lee <seunghl@keti.re.kr>
2021-07-12 13:24:12 +0900

    Implement openpose preprocessing function

--- Openpose/Main.py
+++ Openpose/Main.py
@@ -68,6 +68,13 @@ def preprocess(image):
     mean = torch.Tensor([0.485, 0.456, 0.406]).cuda()
     std = torch.Tensor([0.229, 0.224, 0.225]).cuda()
     device = torch.device('cuda')
+    
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = PIL.Image.fromarray(image)
+    image = transforms.functional.to_tensor(image).to(device)
+    image.sub_(mean[:, None, None]).div_(std[:, None, None])
+    
+    return image[None, ...]
 
 def main():
     if len(sys.argv) == 1 :
