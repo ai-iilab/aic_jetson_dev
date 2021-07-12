1a197ccf97e0c24e69d1780cc90b464a768b1c89
Seungho Lee <seunghl@keti.re.kr>
2021-07-12 11:12:27 +0900

    Implement 'plot_one_box_batch' function

--- Main.py
+++ Main.py
@@ -161,6 +161,29 @@ def plot_one_box(x, img, class_id=None, label=None, line_thickness=None):
             lineType=cv2.LINE_AA,
         )
 
+def plot_one_box_batch(x, img_arr, class_id=None, label=None, line_thickness=None):
+    tl = (
+            line_thickness or round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1
+    )  # line/font thickness
+    # color = color or [random.randint(0, 255) for _ in range(3)]
+    color = text_color[class_id % len(text_color)]
+
+    cv2.rectangle(img, c1, c2, color, thickness=2, lineType=cv2.LINE_AA)
+    if label:
+        tf = max(tl - 1, 1)  # font thickness
+        t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=2)[0]
+        c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
+        #cv2.rectangle(img, c1, c2, color, -1, cv2.LINE_AA)  # filled
+        cv2.putText(
+            img,
+            label,
+            (c1[0], c1[1] - 2),
+            0,
+            tl / 3,
+            color,
+            thickness=2,
+            lineType=cv2.LINE_AA,
+        )
 def save_ap_json(annots):
     # Dump Json
     with open(JSON_PATH, 'w') as json_file:
