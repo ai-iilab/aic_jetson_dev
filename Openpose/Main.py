b99bf958419e1ca4e3a192985fbd6539bc9e330d
Seungho Lee <seunghl@keti.re.kr>
2021-07-12 13:11:39 +0900

    Define functions

--- Openpose/Main.py
+++ Openpose/Main.py
@@ -8,3 +8,12 @@ Authors: Jinwoo Jeong <jw.jeong@keti.re.kr>
 The property of program is under Korea Electronics Technology Institute.
 For more information, contact us at <jw.jeong@keti.re.kr>.
 """
+
+def draw_fps(img, fps):
+
+def preprocess(image):
+
+def main():
+
+if __name__ == "__main__":
+    main()
